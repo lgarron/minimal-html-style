ef35b20fa8041b0e95eacf5eb2e8394f251a07c2
Lucas Garron <code@garron.net>
2022-11-30 03:33:32 -0800

    Add `rome`.

--- script/update-readme.js
+++ script/update-readme.js
@@ -1,8 +1,8 @@
 import { readFile, writeFile } from "node:fs/promises";
 
 const fullHTML = await readFile(
-	new URL("../index.html", import.meta.url),
-	"utf-8",
+  new URL("../index.html", import.meta.url),
+  "utf-8",
 );
 let snippet = fullHTML.match(/.*viewport.*\n/)[0];
 snippet += "  <style>";
@@ -11,8 +11,8 @@ snippet += "</style>";
 console.log(snippet);
 
 const oldReadme = await readFile(
-	new URL("../README.md", import.meta.url),
-	"utf-8",
+  new URL("../README.md", import.meta.url),
+  "utf-8",
 );
 let [newReadme, rest] = oldReadme.split("```html");
 newReadme += "```html\n";
